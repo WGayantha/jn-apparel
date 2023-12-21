8b7f756724bdc2e2ea67de5cc3b9b9b6170f614f
wgayantha <fernandogayantha@gmail.com>
2023-12-21 23:40:17 +0530

    initial commit

---
+++ src/test/java/lk/jnapparel/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.jnapparel.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
